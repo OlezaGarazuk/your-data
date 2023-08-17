115c211981603424a2b890b0999aa2a72a3c826f
Oleg Garazhuk <138308687+OlezaGarazuk@users.noreply.github.com>
2023-08-17 11:30:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,4 @@ class ChatWrapper:
             # If chain is None, that is because no API key was provided.
             if chain is None:
                 history.append((inp, "Please paste your OpenAI key to use"))
+                return history, history
